945aeeb3d84dad77162fe9882ac5e9fdcbd8151c
MaciekBartoszNowakowski <macieknowakowski556@gmail.com>
2024-05-31 15:02:50 +0200

    pole tekstowe

--- src/main/scala/Screen.scala
+++ src/main/scala/Screen.scala
@@ -5,24 +5,51 @@ import scalafx.scene.layout.StackPane
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
 
+import scalafx.scene.control.{Button, TextField}
+import scalafx.scene.layout.{StackPane, VBox}
+
 object Screen extends JFXApp {
   stage = new JFXApp.PrimaryStage {
     title = "Prosty ekran z guzikiem"
     width = 400
     height = 300
-    scene = new Scene {
-      content = new StackPane {
-        children = Seq(
-          new Button {
-            text = "Kliknij mnie!"
-            onAction = new EventHandler[ActionEvent] {
+  scene = new Scene {
+      // Definiujemy textField i button wewnątrz bloku inicjalizacyjnego sceny
+      val textField = new TextField {
+        promptText = "Wpisz tekst tutaj..."
+      }
+
+      val button = new Button {
+        text = "Kliknij mnie!"
+        // onAction = () => println(s"Tekst z TextField: ${textField.text.value}")
+        onAction = new EventHandler[ActionEvent] {
               override def handle(event: ActionEvent): Unit = {
-                println("Przycisk zostal wcisniety!")
-              }
-            }
+                println(textField.text.value)
           }
+        }
+      }
+
+
+      content = new VBox {
+        spacing = 10
+        children = Seq(
+          textField,
+          button
         )
       }
+
+      // content = new StackPane {
+      //   children = Seq(
+      //     new Button {
+      //       text = "Kliknij mnie!"
+      //       onAction = new EventHandler[ActionEvent] {
+      //         override def handle(event: ActionEvent): Unit = {
+      //           println("Przycisk zostal wcisniety!")
+      //         }
+      //       }
+      //     }
+      //   )
+      // }
     }
   }
 }
