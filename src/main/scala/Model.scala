3e5939b56226e59a7bde1d6b5961a3b18be654ba
Kinga Bunkowska <tcx7@o2.pl>
2024-06-03 13:12:59 +0200

    working model

--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -3,12 +3,12 @@ import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{CountVectorizer, Tokenizer}
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.feature.CountVectorizerModel
 import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.functions.{col, split}
-import org.apache.spark.ml.feature.{Tokenizer}
 import org.apache.spark.sql.{SparkSession, Row}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.functions._
 
 class Model(spark: SparkSession){
 
@@ -60,6 +60,13 @@ class Model(spark: SparkSession){
         val predictions = model.transform(featurizedDF)
         predictions.show(5)
 
+        val evaluator = new MulticlassClassificationEvaluator()
+        .setLabelCol("generated")
+        .setPredictionCol("prediction")
+        .setMetricName("accuracy")
+        val accuracy = evaluator.evaluate(predictions)
+        println(s"Training accuracy: $accuracy")
+
 
 }
 
@@ -69,33 +76,47 @@ class Model(spark: SparkSession){
 
     // }
 
-    // def evaluate(test_X: DataFrame, test_Y: DataFrame): Double = {
-    //     // Tokenizacja tekstu w danych testowych
-    //     val tokenizedData = tokenizer.transform(test_X)
-    //     val featurizedData = cvModel.transform(tokenizedData)
+    def evaluate(test: DataFrame): Double = {
+        val featurizedDF = cvModel.transform(tokenizer.transform(test))
+        val predictions = model.transform(featurizedDF)
 
-    //     // Predykcja na danych testowych
-    //     val predictions = model.transform(featurizedData)
+        val evaluator = new MulticlassClassificationEvaluator()
+        .setLabelCol("generated")
+        .setPredictionCol("prediction")
+        .setMetricName("accuracy")
+        val accuracy = evaluator.evaluate(predictions)
 
-    //     // Ewaluacja modelu
-    //     val evaluator = new MulticlassClassificationEvaluator()
-    //     .setLabelCol("label")
-    //     .setPredictionCol("prediction")
-    //     .setMetricName("accuracy")
-    //     val accuracy = evaluator.evaluate(predictions)
+        accuracy
+    }
 
-    //     accuracy
-    // }
-
-    // def predict(text: String): Double = {
+    def predict(text: String): Double = {
         
-    //     val input = spark.createDataFrame(Seq(text))
+        // val input = Seq(text, "other text").toDF("text")
+        // input.show(1)
 
-    //     val tokenizedText = tokenizer.transform(input)
-    //     val featurizedText = cvModel.transform(tokenizedText)
+        val removePunctuation = udf((text: String) => {
+            if (text != null) text.replaceAll("""[\p{Punct}]""", "") else null
+            })
 
-    //     // Predykcja na tekście
-    //     val prediction: Double = model.predict(featurizedText.head().getAs[Vector]("features"))
-    //     prediction
-    // }
+
+        val schema = StructType(Array(StructField("text", StringType, true)))
+
+        // Utworzenie RDD z sekwencji tekstów
+        val rdd = spark.sparkContext.parallelize(Seq(Row(text)))
+
+        // Utworzenie DataFrame z RDD i schematu
+        var input = spark.createDataFrame(rdd, schema)
+        input.show(1)
+
+            input = input.withColumn("text", lower(col("text")))
+                    .withColumn("text", removePunctuation(col("text")))
+                    .withColumn("text", regexp_replace(col("text"), "\n", ""))
+
+        val featurizedDF = cvModel.transform(tokenizer.transform(input))
+        featurizedDF.show(1)
+        val predictions = model.transform(featurizedDF)
+        predictions.show(1)
+
+        predictions.first().getAs[Double]("prediction")
+    }
 }
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -18,13 +18,17 @@ object SimpleApplication {
 
     val model = new Model(spark)
 
-    println("wywolanie train")
     model.train(train)
-    println("zakonczono train")
-    // println("Evalutation")
-    // println(model.evaluate(test_X, test_Y))
+    println("Evalutation")
+    println(model.evaluate(test))
 
-    // println(model.predict("Hello, today we will talk about Scala programming language"))
+    println("Prediction")
+    println(model.predict("""Scala is a coding language short for “Scalable Language.” Many professionals consider Scala to be a modern version of Java, which is another popular programming language. In this article, we’ll explore what Scala is, when you might choose to use it, how it differs from Java, and the steps you can take to learn this exciting language.
+                          While Java has established itself as a top programming language over the years, you might choose to use Scala to address some of Java’s limitations. Scala is a dynamic programming language that easily integrates with the Java platform, yet it offers superior performance, user-friendliness, and coding efficiency. Scala is also open-source, which removes obstacles associated with licensing constraints.
+
+                          As a data scientist or data engineer, you might gravitate toward Scala because of its intrinsic connection with big data. Platforms such as Apache Spark, which is widely used in data analytics and machine learning, use Scala. The straightforward nature of Scala, combined with its inherent type safety, provides an environment that can be beneficial for developing complex data-intensive applications. 
+
+                          Scala is a concise and scalable language compared to many alternatives. As a software programmer, you might choose Scala for the ability to move through software development and testing phases more quickly than when using other languages. Scala is also considered to be flexible when making changes, which can further improve your program development process."""))
 
     // End Spark session
     spark.stop()
